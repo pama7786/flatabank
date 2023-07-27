72095263ddd98db31d3c2d513aec530bcf797254
Abdirahman <abdirahman.diis@student.moringaschool.com>
2023-07-27 10:48:48 +0300

    finished assignment

---
+++ flata/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Trans from './components/transaction';
+import './App.css';
+
+
+function App() {
+
+  return (
+    <div className='main'>
+      <Trans/>
+      
+    </div>
+    
+  );
+}
+
+export default App;
